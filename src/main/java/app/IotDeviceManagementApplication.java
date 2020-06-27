0f6430ac22b8cef6e1a59a396a095695070d009f
DDQ <2841730354@qq.com>
2020-06-27 15:09:58 +0800

    新添加前端资源

--- src/main/java/app/IotDeviceManagementApplication.java
+++ src/main/java/app/IotDeviceManagementApplication.java
@@ -1,9 +1,11 @@
-package app.main;
+package app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "app.dao")
 public class IotDeviceManagementApplication {
 
     public static void main(String[] args) {
